c5329abe175ca6b7930d75a61336de3498c7f075
rover33 <remy.j.pearlstone@gmail.com>
2017-12-21 08:38:48 -0700

    homework

--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -2,16 +2,45 @@ var express = require('express');
 var bodyParser = require('body-parser');
 router = express.Router();
 
-var candies;
+var candies = [{id: 1, name: 'butterfinger', color: 'brown'},
+			   {id: 2, name: 'jawbreaker', color: 'rainbow'},
+			   {id: 3, name: 'nerds', color: 'purple'}
+			]
 
 //What would need to go into candies
 //in order to pass our first test?
-
+//INDEX
 router.get('/', function(req,res) {
+	res.json(candies)
 	// What would go here? 
 	// Hint: we want all candies in JSON format
 });
+//SHOW
+router.get('/:id', function(req,res){
+	res.send(candies[req.params.id -1])
+})
 
-// Fill out the rest of the routes here
+//Create
+router.post('/', function(req,res){
+	candies.push(req.body)
+	res.send(req.body)
+	res.send(candies)
+})
+
+//UPDATE
+
+router.put('/:id', function(req,res){
+	res.send(candies[req.params.id -1] = (req.body))
+	res.send(candies)
+})
+
+//DELETE
+router.delete('/:id', function(req,res){
+	candies[req.params.id -1] = null
+	res.send('You are the weakest link. Goodbye')
+	res.send(candies)
+})
+
+
 
 module.exports = router;
